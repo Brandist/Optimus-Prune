bddb17e731c24e0706daa606f42bc42019f3e327
barendmosch <moschbarend@gmail.com>
2021-11-01 16:43:45 +0100

    data conversion with Eigen Mat and Vec

--- Main/SLAM_API/src/cpp/services/LidarService.cpp
+++ Main/SLAM_API/src/cpp/services/LidarService.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <Eigen/Core>
 #include "../Iservices/ILidarService.h"
 #include "../Ihardware/Lidar.h"
 
@@ -12,8 +13,26 @@ LidarService::LidarService() {
 
 }
 
-// This should be the conversion from raw data to SLAM-readable data (which will be a list of vectors I think)
+// The conversion from raw data to SLAM-readable data (which will be a list of vectors or a matrix)
+// Input: raw Lidar data, from the PCL
+// Output: Matrix or list of vectors containing the x y z coordinates
 int LidarService::convertRawDataToVectors(int raw_data){
+    // raw_data contains all the points in the point cloud, the meta data contains x, y, z coordinates
+    // so to make these into vectors the following will need to be done:
+    // Amount of cols would be the amounts of points right? Amount of sets of x, y z, so the total divided by 3
+    int cols = 1;
+    Eigen::Matrix3Xi mat(3,cols);
+
+    for (int i=0; i<mat.cols(); i++){
+        int x = 10;
+        int y = 10;
+        int z = 10;
+        Eigen::Vector3i vec(x, y, z);
+        mat << vec;
+    }   
+
+    cout << mat << endl;
+    
     return raw_data;
 }
 
