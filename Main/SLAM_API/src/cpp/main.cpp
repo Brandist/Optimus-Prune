393561e9f3abaf971f9e3a3aa910a819befa0666
barendmosch <moschbarend@gmail.com>
2021-11-01 16:39:53 +0100

    disabled SLAM

--- Main/SLAM_API/src/cpp/main.cpp
+++ Main/SLAM_API/src/cpp/main.cpp
@@ -10,11 +10,14 @@ int main() {
     GPSController gps_cont;
     GyroController gyro_cont;
     LidarController lidar_cont;
+    Slam dummy_slam;
 
     int gps_data = gps_cont.requestData();
     int gyro_data = gyro_cont.requestData();
     int lidar_data = lidar_cont.requestData();
 
-    Slam dummy_slam(gps_data, gyro_data, lidar_data);
-    dummy_slam.start();
+    // Is there ROS communication here?
+    // Maybe send the incoming gps, gyro and lidar data to SLAM via ROS bus
+    // then SLAM can read the data, so we dont need to make an instance of SLAM
+    // dummy_slam.start();
 }
