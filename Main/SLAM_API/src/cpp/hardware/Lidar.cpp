6c4bc1dcc89664700e267a8a53a2649d50e3f6b0
barendmosch <moschbarend@gmail.com>
2021-11-01 14:57:24 +0100

    added todo and tested PCL, failed

--- Main/SLAM_API/src/cpp/hardware/Lidar.cpp
+++ Main/SLAM_API/src/cpp/hardware/Lidar.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 #include "../Ihardware/Lidar.h"
 
 using namespace std;
@@ -11,8 +13,16 @@ Lidar::Lidar(){
 // TODO: 
 // - change data type to raw lidar data type
 // - remove set method
+// - replace dummy data with actual point cloud data 
+// - Compile with cmake in order to compile the PCL, see the tutorial
 int Lidar::readRawData(){
     // request the other API for reading data, send the raw data back to service
+    // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    
+    // if (pcl::io::loadPCDFile<pcl::PointXYZ>("test_pcd.pcd", *cloud) == -1) {
+    //     return -1;
+    // }
+    
     setData(10);
     int data = getData();
     return data;
