5aef2cb51a2118f911c59995f22bb81e78bf7b8b
barendmosch <moschbarend@gmail.com>
2021-11-01 11:50:04 +0100

    added SLAM object

--- Main/SLAM_API/src/cpp/main.cpp
+++ Main/SLAM_API/src/cpp/main.cpp
@@ -2,6 +2,7 @@
 #include "Icontrollers/IGPSController.h"
 #include "Icontrollers/IGyroController.h"
 #include "Icontrollers/ILidarController.h"
+#include "SLAM/slam.h"
 
 using namespace std;
 
@@ -10,6 +11,8 @@ int main() {
     GyroController gyro_cont;
     LidarController lidar_cont;
 
-    int d = lidar_cont.requestData();
-    cout << d << endl;
+    int gps_data = gps_cont.requestData();
+    int lidar_data = lidar_cont.requestData();
+
+    Slam dummy_slam(gps_data, lidar_data);
 }
