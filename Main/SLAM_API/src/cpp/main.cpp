3e23f85e639648f2d1ef297323fe844a3240ee9f
barendmosch <moschbarend@gmail.com>
2021-11-01 20:15:50 +0100

    controllers as parameter for SLAM

--- Main/SLAM_API/src/cpp/main.cpp
+++ Main/SLAM_API/src/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "Icontrollers/IGPSController.h"
 #include "Icontrollers/IGyroController.h"
 #include "Icontrollers/ILidarController.h"
@@ -10,14 +11,11 @@ int main() {
     GPSController gps_cont;
     GyroController gyro_cont;
     LidarController lidar_cont;
-    Slam dummy_slam;
-
-    int gps_data = gps_cont.requestData();
-    int gyro_data = gyro_cont.requestData();
-    int lidar_data = lidar_cont.requestData();
+    Slam dummy_slam(gps_cont, gyro_cont, lidar_cont);
 
     // Is there ROS communication here?
     // Maybe send the incoming gps, gyro and lidar data to SLAM via ROS bus
     // then SLAM can read the data, so we dont need to make an instance of SLAM
-    // dummy_slam.start();
+    // Or make an instance of SLAM and throw the controllers in the constructor so SLAM can directly communicate with the controllers
+    dummy_slam.start();
 }
