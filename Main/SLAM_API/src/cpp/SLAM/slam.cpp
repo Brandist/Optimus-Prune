26247b74ed33faa02bc2ae20d3b2a0097f85e422
barendmosch <moschbarend@gmail.com>
2021-11-02 12:20:56 +0100

    reformatted SLAM

--- Main/SLAM_API/src/cpp/SLAM/slam.cpp
+++ Main/SLAM_API/src/cpp/SLAM/slam.cpp
@@ -6,12 +6,18 @@
 #include "../Icontrollers/IGyroController.h"
 #include "../Icontrollers/ILidarController.h"
 
-using namespace std;
+using namespace SLAM;
 
-Slam::Slam(GPSController gps_cont, GyroController gyro_cont, LidarController lidar_cont){
+Slam::Slam(){
+    start();
+}
+
+Slam::Slam(controllers::GPSController gps_cont, controllers::GyroController gyro_cont, controllers::LidarController lidar_cont){
     this->gps_cont = gps_cont;
     this->gyro_cont = gyro_cont;
     this->lidar_cont = lidar_cont;
+
+    start();
 }
 
 bool Slam::start(){
@@ -23,26 +29,31 @@ bool Slam::start(){
     return false;
 }
 
-// TODO:
-// - make classes for the 3 components, each with their needed methods and calculations:
-//     - GPS   
-//     - Gyro
-//     - Lidar
+/* TODO:
+     - make classes for the 3 components, each with their needed methods and calculations:
+     - GPS   
+     - Gyro
+     - Lidar
+     - Maybe one for pure math functions
+     - Visualisation?
+     - Map Making
+     - Also a separate instance of the pathfinding, since our structure tells us that pathfinding starts requests the SLAM map
+*/     
 bool Slam::doSomethingWithGPSData(){
     int data = getGPSData();
-    cout << data << endl;
+    std::cout << data << std::endl;
     return false;
 }
 
 bool Slam::doSomethingWithGyroData(){
     int data = getGyroData();
-    cout << data << endl;
+    std::cout << data << std::endl;
     return false;
 }
 
 bool Slam::doSomethingWithLidarData(){
     Eigen::Matrix3Xf data = getLidarData();
-    cout << data << endl;
+    std::cout << data << std::endl;
     return false;
 }
 
--- Main/SLAM_API/src/cpp/SLAM/slam.h
+++ Main/SLAM_API/src/cpp/SLAM/slam.h
@@ -7,26 +7,33 @@
 #ifndef _slam_h
 #define _slam_h
 
-class Slam {
-    private:
-        GPSController gps_cont;
-        GyroController gyro_cont;
-        LidarController lidar_cont;
-        int gps_data;
-        int gyro_data;
-        Eigen::Matrix3Xf lidar_data;
-    public:
-        Slam(GPSController gps_cont, GyroController gyro_cont, LidarController lidar_cont);
-        bool start();
-        bool doSomethingWithGPSData();
-        bool doSomethingWithLidarData();
-        bool doSomethingWithGyroData();
-        void setGPSData(int gps_data);
-        int getGPSData();
-        void setLidarData(Eigen::Matrix3Xf lidar_data);
-        Eigen::Matrix3Xf getLidarData();
-        void setGyroData(int gyro_data);
-        int getGyroData();
-};
+/* TODO:
+    - change data types of gps and gyro
+    - think about whats private and public, maybe even protected
+*/
+namespace SLAM{
+    class Slam {
+        private:
+            controllers::GPSController gps_cont;
+            controllers::GyroController gyro_cont;
+            controllers::LidarController lidar_cont;
+            int gps_data;
+            int gyro_data;
+            Eigen::Matrix3Xf lidar_data;
+            void setGPSData(int gps_data);
+            void setGyroData(int gyro_data);
+            void setLidarData(Eigen::Matrix3Xf lidar_data);
+            bool start();
+        public:
+            Slam();
+            Slam(controllers::GPSController gps_cont, controllers::GyroController gyro_cont, controllers::LidarController lidar_cont);
+            bool doSomethingWithGPSData();
+            bool doSomethingWithLidarData();
+            bool doSomethingWithGyroData();
+            int getGPSData();
+            int getGyroData();
+            Eigen::Matrix3Xf getLidarData();
+    };
+}
 
 #endif
