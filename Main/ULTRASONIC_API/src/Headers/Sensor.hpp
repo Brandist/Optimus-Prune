45ec77574b7ec7ef5a04789e85ed8e4bc1642fc0
Caelia00 <d.v.langen@gmail.com>
2021-11-08 21:59:51 +0100

    Removed ReadPulse and added unit test

--- Main/ULTRASONIC_API/src/Headers/Sensor.hpp
+++ Main/ULTRASONIC_API/src/Headers/Sensor.hpp
@@ -2,7 +2,8 @@
 #define _ultrasonic_h
 
 //library for the gpio of the pi.
-#include<pigpio>
+#include<pigpio.h>
+#include<iostream>
 
 class Sensor
 {
@@ -10,18 +11,19 @@ private:
     int listenPin;
     int triggerPin;
 
-    static uint32_t rise_tick = 0;    // Pulse rise time tick value
-    static uint32_t pulse_width = 0;  // Last measured pulse width (us)
+    
 
-    void Trigger();
-    void ReadPulse(int pi, unsigned user_gpio, unsigned level, uint32_t tick);
+    bool Trigger();
+    //void ReadPulse(int gpio, int level, uint32_t tick);
 
     
 public:
-    Sensor(/* args */);
+    Sensor(int,int);
     ~Sensor();
     float ReadSensor();
-    
+    std::string Sensor::UnitTest();
+    //void SetRiseTick(int);
+   // uint32_t GetRiseTick();
 };
 
 
