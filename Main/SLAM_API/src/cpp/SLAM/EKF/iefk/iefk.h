98af9a75ef8bf92e935213f345a410e1f2309fe8
barendmosch <moschbarend@gmail.com>
2021-11-09 21:04:40 +0100

    skeleton EKF Odometry Landmarks

---
+++ Main/SLAM_API/src/cpp/SLAM/EKF/iefk/iefk.h
@@ -0,0 +1,23 @@
+#include <Eigen/Core>
+#include "../../mapping/imapping/Imap.h"
+
+#ifndef _ekf_h
+#define _ekf_h
+
+namespace EKF{
+    class Ekf {
+        private:
+            map::Map slam_map;
+            Eigen::Vector3f robot_position;
+            void setRobotPosition(Eigen::Vector3f robot_position);
+        public:
+            Ekf();
+            void init();
+            void performOdometry();
+            void performLandmarkExtraction();
+            void updateRobotPosition();
+            Eigen::Vector3f getRobotPosition();
+    };
+}
+
+#endif
