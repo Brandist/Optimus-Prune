a17edef7e93141e86a1ef392b842d0e999dabb03
barendmosch <moschbarend@gmail.com>
2021-11-09 21:02:29 +0100

    added SLAM init and start

--- Main/SLAM_API/src/cpp/main.cpp
+++ Main/SLAM_API/src/cpp/main.cpp
@@ -7,14 +7,18 @@
 
 using namespace std;
 
+/* Is there ROS communication here?
+ Maybe send the incoming gps, gyro and lidar data to SLAM via ROS bus
+ then SLAM can read the data, so we dont need to make an instance of SLAM
+ Or make an instance of SLAM and throw the controllers in the constructor so SLAM can directly communicate with the controllers */
 int main() {
     controllers::GPSController gps_cont;
     controllers::GyroController gyro_cont;
     controllers::LidarController lidar_cont;
-    SLAM::Slam dummy_slam(gps_cont, gyro_cont, lidar_cont);
+    SLAM::Slam slam(gps_cont, gyro_cont, lidar_cont);
 
-    // Is there ROS communication here?
-    // Maybe send the incoming gps, gyro and lidar data to SLAM via ROS bus
-    // then SLAM can read the data, so we dont need to make an instance of SLAM
-    // Or make an instance of SLAM and throw the controllers in the constructor so SLAM can directly communicate with the controllers
+    /* Slam should run indefinitely, (untill the robot stops that is)
+        Atm, this is 1 iteration, which takes in 1 set of data and makes 1 map (submap of the whole field) */
+    slam.init();
+    slam.start();
 }
