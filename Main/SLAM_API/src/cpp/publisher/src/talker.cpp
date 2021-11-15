b466ce4e66d10a31418a565cab6eec7c0eba160d
Barend Mosch <moschbarend@gmail.com>
2021-11-15 17:03:50 +0100

    msg initialised with vector data from lidar service

--- Main/SLAM_API/src/cpp/publisher/src/talker.cpp
+++ Main/SLAM_API/src/cpp/publisher/src/talker.cpp
@@ -3,6 +3,7 @@
 #include "apilib/Icontrollers/ILidarController.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "publisher/someMsg.h"
 #include <Eigen/Core>
 
 using namespace std;
@@ -16,25 +17,37 @@ int main(int argc, char **argv) {
     controllers::LidarController lidar_cont;
 
     int g_data = gps_cont.requestData();
-    Eigen::Matrix3Xf l_data = lidar_cont.requestData();
+    // Eigen::Matrix3Xf l_data = lidar_cont.requestData();
 
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
 
-    ros::Publisher gps_data = n.advertise<std_msgs::String>("gps", 1000);
+    ros::Publisher gps_data = n.advertise<publisher::someMsg>("gps", 1000);
     ros::Publisher lidar_data = n.advertise<std_msgs::String>("lidar", 1000);
 
     ros::Rate loop_rate(10);
 
     int count=0;
-
+    publisher::someMsg msg;
     while(ros::ok()){
-        std_msgs::String msg;
-        stringstream ss;
-        ss << "hello peeps " << count;
-        msg.data = ss.str();
+        Eigen::Matrix3Xf l_data = lidar_cont.requestData();
 
-        ROS_INFO("%s ", msg.data.c_str());
+        std::vector<double> x_a, y_a, z_a;
+
+        for (int i=0; i<l_data.cols(); ++i){
+            x_a.push_back(l_data.col(i).x());
+            y_a.push_back(l_data.col(i).y());
+            z_a.push_back(l_data.col(i).z());
+        }
+
+        msg.x = x_a;
+        msg.y = y_a;
+        msg.z = z_a;
+        
+        // print x, y, z first point
+        ROS_INFO("%f ", msg.x[0]);
+        ROS_INFO("%f ", msg.y[0]);
+        ROS_INFO("%f ", msg.z[0]);
 
         gps_data.publish(msg);
         ros::spinOnce();
