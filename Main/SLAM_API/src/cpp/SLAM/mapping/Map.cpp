229919bb86f898a397993331e4de278cc2e14f67
Brandon Disseldorp <brandondisseldorp@hotmail.com>
2021-11-15 13:29:50 +0100

    purge main
    need an empty main

--- Main/SLAM_API/src/cpp/SLAM/mapping/Map.cpp
+++
@@ -1,3 +0,0 @@
-// Reading the data from the Lidar and GPS (IMU) and putting the coordinates a map
-// Maybe separate files for landmark extraction, odometry and EKF?
-// This still need to be worked out
