00819b4137b867593e6a8faee1fe75257a5421c0
Barend Mosch <moschbarend@gmail.com>
2021-11-16 17:08:33 +0100

    working lidar data topic and sending data to SLAM

--- Main/SLAM_API/src/cpp/publisher/src/talker.cpp
+++ Main/SLAM_API/src/cpp/publisher/src/talker.cpp
@@ -3,7 +3,7 @@
 #include "apilib/Icontrollers/ILidarController.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "publisher/someMsg.h"
+#include "publisher/lidar.h"
 #include <Eigen/Core>
 
 using namespace std;
@@ -17,22 +17,22 @@ int main(int argc, char **argv) {
     controllers::LidarController lidar_cont;
 
     int g_data = gps_cont.requestData();
-    // Eigen::Matrix3Xf l_data = lidar_cont.requestData();
 
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
 
-    ros::Publisher gps_data = n.advertise<publisher::someMsg>("gps", 1000);
-    ros::Publisher lidar_data = n.advertise<std_msgs::String>("lidar", 1000);
+    // TODO: add second topic for the GPS, which separate msg
+    // ros::Publisher gps_data = n.advertise<publisher::lidar>("gps", 1000);
+    ros::Publisher lidar_data = n.advertise<publisher::lidar>("lidar_topic", 1000);
 
     ros::Rate loop_rate(10);
 
     int count=0;
-    publisher::someMsg msg;
+    publisher::lidar msg;
     while(ros::ok()){
         Eigen::Matrix3Xf l_data = lidar_cont.requestData();
 
-        std::vector<double> x_a, y_a, z_a;
+        std::vector<float> x_a, y_a, z_a;
 
         for (int i=0; i<l_data.cols(); ++i){
             x_a.push_back(l_data.col(i).x());
@@ -44,12 +44,9 @@ int main(int argc, char **argv) {
         msg.y = y_a;
         msg.z = z_a;
         
-        // print x, y, z first point
-        ROS_INFO("%f ", msg.x[0]);
-        ROS_INFO("%f ", msg.y[0]);
-        ROS_INFO("%f ", msg.z[0]);
+        // ROS_INFO("%f ", msg.x[0]);
 
-        gps_data.publish(msg);
+        lidar_data.publish(msg);
         ros::spinOnce();
 
         loop_rate.sleep();
--- Main/SLAM_API/src/cpp/subscriber/src/listener.cpp
+++ Main/SLAM_API/src/cpp/subscriber/src/listener.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "subscriber/lidar.h"
 #include "slam/Slam.h"
 
 using namespace std;
 
-void chatterCallBack(const std_msgs::String::ConstPtr& msg){
-    ROS_INFO("I heard: [%s] ", msg->data.c_str());
+SLAM::Slam slam;
+
+void chatterCallBack(const subscriber::lidar::ConstPtr& msg){
+    // ROS_INFO("I heard: [%f] ", msg->x[0]);
+    slam.initLidarData(msg->x, msg->y, msg->z);
+    slam.start();
 }
 
 int main(int argc, char **argv){
-
     ros::init(argc, argv, "listener");
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("gps", 1000, chatterCallBack);
+    slam.init();
+
+    ros::Subscriber sub = n.subscribe("lidar_topic", 1000, chatterCallBack);
 
     ros::spin();
 
