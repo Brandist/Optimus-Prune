85efb48310e678a5d19b73143244ac5f4041adec
Barend Mosch <moschbarend@gmail.com>
2021-12-14 17:36:44 +0100

    changed pcd input

--- Main/SLAM_API/src/cpp/apilib/include/apilib/Iservices/ILidarService.h
+++ Main/SLAM_API/src/cpp/apilib/include/apilib/Iservices/ILidarService.h
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <string>
 #include <Eigen/Core>
-#include <pcl/io/pcd_io.h>
+#include <pcl/io/png_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/io/point_cloud_image_extractors.h>
 #include <pcl/point_types.h>
+#include <pcl/PCLImage.h>
+#include <pcl/io/file_io.h>
+#include <pcl/io/image.h>
+#include <pcl/common/common.h>
 #include "apilib/Ihardware/Lidar.h"
 
 #ifndef _lidarservice_h
